f6d8ed2b78d0858709de424a604bf8fc36b48698
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2021-12-19 21:00:46 +0800

    Add files via upload

---
+++ Hotel Reservation System/src/hotel/Customer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel;
+
+/**
+ *
+ * @author jason
+ */
+public class Customer {
+    protected String custID;
+    protected String custName;    
+}
