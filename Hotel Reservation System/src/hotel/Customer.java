ca6ec5a0de21194fb92a8a1982b951b3a18a49c9
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2021-12-29 01:39:46 +0800

    Update Customer.java

--- Hotel Reservation System/src/hotel/Customer.java
+++ Hotel Reservation System/src/hotel/Customer.java
@@ -9,69 +9,67 @@ package hotel;
  * @author jason
  */
 public class Customer {
-    protected String cust_id[];
-    protected String cust_name[];
-    protected String cust_numphone[];
-    protected String cust_email[];
+    protected String cust_id;
+    protected String cust_name;
+    protected String cust_numphone;
+    protected String cust_email;
     protected int room_num;
     protected String room_type;
 
     /**
      * @return the cust_id
      */
-    public String[] getCust_id() {
+    public String getCust_id() {
         return cust_id;
     }
 
     /**
      * @param cust_id the cust_id to set
      */
-    public void setCust_id(String[] cust_id) {
+    public void setCust_id(String cust_id) {
         this.cust_id = cust_id;
     }
 
     /**
      * @return the cust_name
      */
-    public String[] getCust_name() {
+    public String getCust_name() {
         return cust_name;
     }
 
     /**
      * @param cust_name the cust_name to set
      */
-    public void setCust_name(String[] cust_name) {
+    public void setCust_name(String cust_name) {
         this.cust_name = cust_name;
     }
 
     /**
      * @return the cust_numphone
      */
-    public String[] getCust_numphone() {
+    public String getCust_numphone() {
         return cust_numphone;
     }
 
     /**
      * @param cust_numphone the cust_numphone to set
      */
-    public void setCust_numphone(String[] cust_numphone) {
+    public void setCust_numphone(String cust_numphone) {
         this.cust_numphone = cust_numphone;
     }
 
     /**
      * @return the cust_email
      */
-    public String[] getCust_email() {
+    public String getCust_email() {
         return cust_email;
     }
 
     /**
      * @param cust_email the cust_email to set
      */
-    public void setCust_email(String[] cust_email) {
+    public void setCust_email(String cust_email) {
         this.cust_email = cust_email;
     }
-    
-    
-    
+
 }
