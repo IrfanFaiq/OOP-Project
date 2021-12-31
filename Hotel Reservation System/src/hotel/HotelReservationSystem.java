85567bef263a39a7049991bc0cf94e401427ae9e
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2021-12-31 22:19:25 +0800

    Update HotelReservationSystem.java

--- Hotel Reservation System/src/hotel/HotelReservationSystem.java
+++ Hotel Reservation System/src/hotel/HotelReservationSystem.java
@@ -16,10 +16,17 @@ public class HotelReservationSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
         int n;
         
-        Customer[] customer = new Customer[10];
+        Room[] customer = new Room[10];
+        Booking booking = new Booking();
+        Customer cust2 = new Room();
+//        HolidayPackage cus2 = new HolidayPackage();//test new code line
+//       HolidayPackage hol_pack = new HolidayPackage();//test new code line
+        Payment cust1 = new Payment(booking);
+        Payment cus = new Payment(booking);//test new code line
+//        Payment cuz = new Payment (hol_pack);//test new code line
 
       
         Scanner input = new Scanner (System.in);
@@ -40,7 +47,7 @@ public class HotelReservationSystem {
             
             for (int i=0;i<n;i++){
                 System.out.println("Name "+(i+1) + ":");
-                customer[i]= new Customer();
+                customer[i]= new Room();
                 customer[i].setCust_name(input.next());
                 
                 System.out.println("Phone Number "+(i+1)+":");
@@ -54,6 +61,27 @@ public class HotelReservationSystem {
                 System.out.println("Phone Number:" +customer[i].getCust_numphone());
                 System.out.println("Email:" +customer[i].getCust_email());
             }
+            
+            System.out.println("How many days:");
+            booking.setBook_days(input.nextInt());
+            
+            System.out.println("Choose Room type:");
+            System.out.println("A = Deluxe \nB = Premium \nC = Normal");
+            cust2.setRoom_type(input.next());            
+            //new code line
+            System.out.println("Do you want holiday package?: ");            
+            System.out.println("1. Yes/ 2. No");
+            int b = input.nextInt();
+            if(b == 2)
+            {
+                System.out.println(cust2.getRoom_price());
+                System.out.println("RM " + cust1.CalculatePrice());
+            }
+            else{
+                System.out.println(cust2.getRoom_price());
+                System.out.println("RM " + cus.CalculatePricePromo());
+            }
+                                    
             }
 
         
@@ -64,5 +92,5 @@ public class HotelReservationSystem {
             System.out.println("\nError command not recognize, Please Try Aggain");
         }
     }
-    
 }
+
