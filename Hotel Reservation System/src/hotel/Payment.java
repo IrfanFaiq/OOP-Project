686c95974dea21c93124ae16c97153de7abdc3ee
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2021-12-31 22:22:38 +0800

    Update Payment.java

--- Hotel Reservation System/src/hotel/Payment.java
+++ Hotel Reservation System/src/hotel/Payment.java
@@ -8,14 +8,40 @@ package hotel;
  *
  * @author jason
  */
-public class Payment {
-    public float payment_amount;
-    public String payment_date;  
-    public Booking booking;
+public class Payment extends Customer{
+
+    public String payment_date;
+  
+   
     
-    public double CalculatePrice(){
-        
-    return payment_amount = booking.getBook_days() * 120;}
-    
-    
+    public Payment (Booking booking){
+    this.booking = booking;
 }
+/*    public Payment (HolidayPackage hol_pack){
+        this.hol_pack = hol_pack;
+    }*///test new code
+            
+    public double CalculatePrice(){
+        total_payment= booking.getBook_days() * 100;
+        return getTotal_payment();
+    }
+    
+    public double CalculatePricePromo(){
+       // total_payment= (booking.getBook_days() * 100)* hol_pack.getPackage_price();//test new code
+        return getTotal_payment();
+    }
+
+    /**
+     * @return the booking
+     */
+    public Booking getBooking() {
+        return booking;
+    }
+
+    /**
+     * @param booking the booking to set
+     */
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+ }
