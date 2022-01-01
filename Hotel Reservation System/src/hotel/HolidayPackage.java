68b51533dcfdcb028cf743b4e3533d063ba99536
IrfanFaiq <93464504+IrfanFaiq@users.noreply.github.com>
2022-01-01 18:46:55 +0800

    Update HolidayPackage.java

--- Hotel Reservation System/src/hotel/HolidayPackage.java
+++ Hotel Reservation System/src/hotel/HolidayPackage.java
@@ -10,4 +10,40 @@ package hotel;
  */
 public class HolidayPackage {
     public double package_price = 0.2;
+    public String suggest;
+
+    /**
+     * @return the suggest
+     */
+    public String getSuggest() {
+        return suggest;
+    }
+
+    /**
+     * @param suggest the suggest to set
+     */
+    public void setSuggest(String suggest) {
+        this.suggest = suggest;
+    }
+}
+
+
+interface moreOption{
+    public void holiday();
+    public void suggestion();
+    public void gratitiude();
+    
+}
+
+class Option implements moreOption{
+    public void holiday(){
+        System.out.println("Thank you for choosing our holiday package");
+    }
+    public void suggestion(){
+        System.out.println("We appreciate your suggestions and will do our best for future customer satisfaction");
+    }
+    public void gratitiude(){
+        System.out.println("Thanks for choosing our hotel and have a nice day");
+    }
+    
 }
